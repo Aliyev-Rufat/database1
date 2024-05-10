94efb002f5cd694aad02ec64b1b92ec70192ed9d
Aliyev-Rufat <Rufat0747@gmail.com>
2024-05-10 15:00:19 +0400

    taskda sehv foldere yuklemisdim muellim ona gore ata bilmirdim

---
+++ Program.cs
@@ -0,0 +1,33 @@
+namespace WebApplication6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+         
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+          );
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
